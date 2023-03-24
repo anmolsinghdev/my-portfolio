fda07f27d416387d2ca70807b3f3b9b76ce05a77
ohboy.web@gmail.com <anmolsingh>
2023-03-24 12:46:49 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,180 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from './assets/logo.svg';
+import logo from './assets/logo2.svg';
+// Profile Image
+import profile from './assets/profile.jpg';
+// Tech stack images
+import html from './assets/techstack/html.png';
+import css from './assets/techstack/css.png';
+import sass from './assets/techstack/sass.png';
+import js from './assets/techstack/js.png';
+import react from './assets/techstack/react.png';
+import redux from './assets/techstack/redux.png';
+import tailwind from './assets/techstack/tailwind.png';
+import bootstrap from './assets/techstack/bootstrap.png';
+import vscode from './assets/techstack/vscode.png';
+import github from './assets/techstack/github.png';
+import git from './assets/techstack/git.png';
+import npm from './assets/techstack/npm.png';
+import postman from './assets/techstack/postman.png';
+import nodejs from './assets/techstack/nodejs.png';
+import express from './assets/techstack/express.png';
+import mongodb from './assets/techstack/mongodb.png';
+// import figma from './assets/techstack/figma.png';
+// Porject Images
+import projectImage1 from './assets/projects/project1.jpg';
+import projectImage2 from './assets/projects/project2.jpg';
+import projectImage3 from './assets/projects/project3.jpg';
+import projectImage4 from './assets/projects/project4.jpg';
+import projectImage5 from './assets/projects/project5.jpg';
+import projectImage6 from './assets/projects/project6.jpg';
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: 'Anmol Singh',
+  tagline: 'I build things for web',
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: 'https://www.linkedin.com/in/anmol-singh-a29218183/',
+  github: 'https://github.com/webohboy',
+  twitter: 'https://twitter.com/_anmol_chauhan',
+  instagram: 'https://www.instagram.com/anmolchauhan._/',
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: 'MERN STACK Web Developer',
+    Company: `Chetu.inc`,
+    Location: 'Noida',
+    Type: 'Full Time',
+    Duration: 'Jan 2022 - Till Now',
+  },
+  {
+    Position: 'Training',
+    Company: `Chetu.inc`,
+    Location: 'Noida',
+    Type: 'Training',
+    Duration: 'Sept 2021 - Jan 2022',
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: 'Backend Development',
+    Company: 'Chetu.inc',
+    Location: 'Noida',
+    Type: 'Full Time',
+    Duration: 'Sept 2021 - Jan 2022',
+  },
+  {
+    Position: 'Frontend Development',
+    Company: 'Udemy, YouTube, Google',
+    Location: 'Online',
+    Type: 'Full Time',
+    Duration: 'Sept 2021 - Jan 2022',
+  },
+  {
+    Position: `Bachelors's in Computer Application`,
+    Company: `I.T.S Mohan Nagar, Ghaziabad`,
+    Location: 'Uttar Pradesh',
+    Type: 'Full Time',
+    Duration: 'Aug 2018 - June 2021',
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  nodejs: nodejs,
+  express: express,
+  mongodb: mongodb,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: 'Project title 1',
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 2',
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 3',
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 4',
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 5',
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+  {
+    title: 'Project title 6',
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+    project lorem ipsum generator for dummy content`,
+    techstack: 'HTML/CSS, JavaScript',
+    previewLink: 'https://google.com',
+    githubLink: 'https://github.com',
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: 'hi@example.com',
+  phone: '+91 8929271223',
+};
