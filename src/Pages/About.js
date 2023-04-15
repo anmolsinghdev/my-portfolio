0348878e82c8b2d73c7710dd3ef9decc30badda4
ohboy.web@gmail.com <anmolsingh>
2023-04-15 22:29:45 +0530

    adding transitions on about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Work from '../Components/Work';
 import { personalDetails, workDetails, eduDetails } from '../Details';
+import { gsap } from 'gsap';
 
 function About() {
+  const sectionOne = useRef();
+  const sectionTwo = useRef();
+  const sectionThree = useRef();
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(
+      sectionOne.current,
+      {
+        y: '-100%',
+        delay: 0.5,
+        opacity: 0,
+        duration: 2,
+        ease: 'Power3.easeOut',
+      },
+      '<'
+    )
+      .from(
+        sectionTwo.current,
+        {
+          x: '-100%',
+          delay: 0.2,
+          opacity: 0,
+          duration: 2,
+          ease: 'Power3.easeOut',
+        },
+        '<'
+      )
+      .from(
+        sectionThree.current,
+        {
+          y: '100%',
+          delay: 0.2,
+          opacity: 0,
+          duration: 2,
+          ease: 'Power3.easeOut',
+        },
+        '<'
+      );
+  }, []);
   return (
     <main className="container mx-auto max-width pt-10 pb-20 ">
-      <section>
+      <section className="section-1" ref={sectionOne}>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           About Me
         </h1>
@@ -13,7 +53,7 @@ function About() {
           {personalDetails.about}
         </p>
       </section>
-      <section>
+      <section ref={sectionTwo}>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           Work Experience
         </h1>
@@ -29,7 +69,7 @@ function About() {
           ))
         )}
       </section>
-      <section>
+      <section ref={sectionThree}>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           Education
         </h1>
